e4222e8e0a62e18d7469113f76174f398352bdd3
admin <ambrus.attila01@gmail.com>
2019-05-09 15:01:02 +0200

    Init

---
+++ JWTLogin/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JWTLogin
+//
+//  Created by Ati on 2019. 05. 04..
+//  Copyright © 2019. Ati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
